6e4406bf495f4128dcd340762ca9c3bd80d1db6c
Matthew Tole <matthewtole@gmail.com>
2012-09-25 11:59:47 -0700

    Update Gecko to latest version, with all new functions.

--- js/gecko.js
+++ js/gecko.js
@@ -10,6 +10,11 @@ var gecko = (function () {
     });
   };
 
+  // Set the title of the widget.
+  me.setTitle = function(title) {
+    chameleon.setTitle({ text: title });
+  };
+
   // Reload the entire widget code.
   me.reloadWidget = function() {
     chameleon.refresh({ reload: true });
@@ -24,6 +29,43 @@ var gecko = (function () {
   me.hideLoading = function() {
     chameleon.showLoading({ showloader: false });
   };
+
+  // Get the value of a single entry in the widget instance data.
+  // If the entry doesn't exist, the default value will be returned.
+  me.getInstanceData = function(key, def) {
+    var data = _getInstanceData(); 
+    if (data[key]) {
+      return data[key];
+    }
+    if (def) {
+      return def;
+    }
+    return null;
+  }
+
+  // Set the value of a single entry in the widget instance data.
+  me.setInstanceData = function(key, value) {
+    var data = _getInstanceData();
+    data[key] = value;
+    _saveInstanceData(data);
+  }
+
+  // ## Private Functions
+
+  // Get the data object for this widget instance, creating it if it
+  // currently doesn't exist.
+  var _getInstanceData = function() {
+    var data = chameleon.getData();
+    if (data == null) {
+      data = {};
+    }
+    return data;
+  }
+
+  // Save the _data_ object into the widget instance data storage.
+  var _saveInstanceData = function(data) {
+    chameleon.saveData(data);
+  }
 
   return me;
 
